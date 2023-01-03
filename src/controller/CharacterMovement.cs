dfc0d5f5678bb3700cff58a30c6c887ddff6049b
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2023-01-03 14:54:51 +0000

    feat: character controller (keyboard and mouse currently)

---
+++ src/controller/CharacterMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+  /// <value>Property <c>tf</c> represents the <see cref="UnityEngine.CoreModule.Transform"> component.</value>
+  private Transform tf;
+
+  /// <value>The <c>speed</c> of the movement.</value>
+  [field: SerializeField] public float speed = 5f;
+
+
+  /// <summary>
+  /// Moves itself in the XZ plane.
+  /// </summary>
+  private void Move() {
+    float z_movement = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+    float x_movement = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+    tf.position += tf.forward * z_movement + tf.right * x_movement;
+  }
+
+  // Start is called before the first frame update
+  void Start() {
+    tf = GetComponent<Transform>();
+  }
+
+  /// <summary>
+  /// Moves the object normally with the arrow keys
+  /// </summary>
+  void Update() {
+    Move();
+  }
+}
