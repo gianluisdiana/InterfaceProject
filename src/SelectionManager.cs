3ca12793cf5dbcbc14316ac625873a6b94348b42
Jorge Quintana García <alu0101123547@ull.edu.es>
2023-01-10 17:22:04 +0000

    Add files via upload

---
+++ src/SelectionManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    public float pickUpRange = 5f;
+
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+    private Transform _selection;
+    
+    private void Update() {
+        if (_selection != null) {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+        
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hit, pickUpRange)) {
+            var selection = hit.transform;
+            if (selection.CompareTag("canPickUp")) {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null) {
+                    selectionRenderer.material = highlightMaterial;
+                }
+
+                _selection = selection;
+            }
+        }
+    }
+}
