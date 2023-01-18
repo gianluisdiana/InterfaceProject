8da4efe2aab1fa2597fd5f09528c99975a186552
Gian Luis Bolivar Diana <alu0101336046@ull.edu.es>
2023-01-18 17:19:51 +0000

    feat: display help to the user

---
+++ src/DisplayHelp.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic; // Dictionary
+using UnityEngine;
+using TMPro; // TMP_Text
+
+[RequireComponent(typeof (PickUpScript))]
+public class DisplayHelp : MonoBehaviour {
+  // ------------------------- Serialized Values -------------------------- //
+
+  [Tooltip("The text object where will be displayed the help")]
+  [SerializeField] private TMP_Text commonHelpText;
+
+  [Tooltip("The script that will notify when an object is grabbed.")]
+  [SerializeField] private PickUpScript pickNotifier;
+
+  [Tooltip("The script that will notify when an object is highlighted")]
+  [SerializeField] private SelectionManager selectionNotifier;
+
+  // ------------------------- Private attributes ------------------------- //
+
+  /// <value> String with help on how to grab an object. </value>
+  private string pickHelp = "Press 'G' to pick up an object";
+
+  /// <value> Keeps track if the pick help was already displayed. </value>
+  private bool pickHelpAlreadyDisplayed = false;
+
+  /// <value> String with help on how to throw an object. </value>
+  private string throwHelp = "Press 'T' to throw the object.";
+
+  /// <value> String with help on how to drop an object. </value>
+  private string dropHelp = "Press 'R' to drop the object.";
+
+  /// <value> Keeps track if the 'drop and throw' help was already displayed.</value>
+  private bool dropThrowHelpAlreadyDisplayed = false;
+
+  // -------------------------- Private Methods --------------------------- //
+
+  /// <summary>
+    /// Display help the first time the user points to a pick-able object.
+  /// </summary>
+  private void displayPickHelp() {
+    // Only display it once
+    if (this.pickHelpAlreadyDisplayed) return;
+
+    this.pickHelpAlreadyDisplayed = true;
+    // If there's any text, wait till it disappears
+    while (this.commonHelpText.text != "") yield return new WaitForSeconds(0.5f);
+
+    this.commonHelpText.text = this.pickHelp;
+    // Keep the text 5 seconds
+    yield return new WaitForSeconds(5f);
+    this.commonHelpText.text = "";
+  }
+
+  /// <summary>
+    /// Display help the first time the user is holding an object.
+  /// </summary>
+  private void displayDropThrowHelp() {
+    // Only display it once
+    if (this.dropThrowHelpAlreadyDisplayed) return;
+
+    this.dropThrowHelpAlreadyDisplayed = true;
+    // If there's any text, wait till it disappears
+    while (this.commonHelpText.text != "") yield return new WaitForSeconds(0.5f);
+
+    this.commonHelpText.text = this.dropHelp + "\n" + this.throwHelp;
+    // Keep the text 5 seconds
+    yield return new WaitForSeconds(5f);
+    this.commonHelpText.text = "";
+  }
+
+  private void Start() {
+    this.selectionNotifier.OnHighlight += displayPickHelp;
+    this.pickNotifier.OnGrab += displayDropThrowHelp;
+  }
+}
