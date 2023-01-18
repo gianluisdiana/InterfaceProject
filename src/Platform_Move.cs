93ad5354dff9d0d76d39a311113fe66c0d937a17
Gian Luis Bolivar Diana <alu0101336046@ull.edu.es>
2023-01-18 22:42:27 +0000

    doc: last script commented (almost)
    Will be finished by Marta

--- src/Platform_Move.cs
+++ src/Platform_Move.cs
@@ -2,71 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Platform_Move : MonoBehaviour
-{
+public class Platform_Move : MonoBehaviour {
+    // ------------------------- Serialized values ------------------------- //
+
+    [Tooltip("")]
+    [SerializeField] private Transform parent;
+
+    [Tooltip("")]
+    [SerializeField] private Transform oldParent;
+
+    // ------------------------- Private attributes ------------------------- //
+
+    /// <value>  </value>
     private bool goForward;
+
+    /// <value>  </value>
     private bool goLeft;
+
+    /// <value>  </value>
     private bool finalPos;
 
+    /// <value>  </value>
     private GameObject box;
 
-    public Transform parent;
+    /// <value>  </value>
+    private Vector3 initialPosition;
 
-    public Transform oldParent;
+    /// <value>  </value>
+    private Vector3 frontPosition;
 
-    private Vector3 initialPosition = new Vector3(-1, 1.2F, -2.75F); // Posición inicial
+    /// <value>  </value>
+    private Vector3 finalPosition;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        goForward = true;
-        finalPos = false;
-        goLeft = false;
-        box = GameObject.FindWithTag("canPickUp");
-        
-    }
+    // ---------------------------- Private Method --------------------------- //
 
-    void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.tag == box.tag) {
-            box.transform.SetParent(parent);
-            Debug.Log("Colision caja");
-            move();
+    /// <summary>
+        ///
+    /// </summary>
+    void move() {
+        Vector3 currentPosition = transform.position;
+
+        if (finalPos) return;
+
+        if (goForward) {
+            if (currentPosition.z < frontPosition.z) {
+                transform.Translate(Vector3.forward * 0.01f);
+            } else {
+                goForward = false;
+                goLeft = true;
+            }
+        } else if (goLeft) {
+            if (currentPosition.x > finalPosition.x) {
+                transform.Translate(Vector3.left * 0.01f);
+            } else {
+                goLeft = false;
+            }
+        } else {
+            transform.Rotate(0, 0, 90);
+            finalPos = true;
         }
     }
 
-    void OnCollisionExit()
-    {
+    // ----------------------------- Unity methods ----------------------------- //
+
+    /// <summary>
+        ///
+    /// </summary>
+    void Start() {
+        goForward = true;
+        goLeft = false;
+        finalPos = false;
+        box = GameObject.FindWithTag("canPickUp");
+
+        initialPosition =  = new Vector3(-1, 1.2f, -2.75f);
+        frontPosition = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z + 5.5f);
+        finalPosition = new Vector3(initialPosition.x - 2, initialPosition.y, initialPosition.z);
+    }
+
+    /// <summary>
+        ///
+    /// </summary>
+    /// <param name="collision"> The object that collided with. </param>
+    void OnCollisionStay(Collision collision) {
+        if (collision.gameObject.tag != box.tag) return;
+        box.transform.SetParent(parent);
+        move();
+    }
+
+    /// <summary>
+        ///
+    /// </summary>
+    void OnCollisionExit() {
         box.transform.SetParent(oldParent);
         transform.position = initialPosition;
         goForward = true;
         goLeft = false;
-    }
-
-    void move()
-    {
-        Vector3 currentPosition = transform.position; //Posición a tiempo real
-        Vector3 frontPosition = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z + 5.5F); //Posición al frente
-        Vector3 finalPosition = new Vector3(initialPosition.x-2, initialPosition.y, initialPosition.z); //Posición al frente
-
-        if(!finalPos){
-            if (goForward) {
-                if (currentPosition.z < frontPosition.z) {
-                    transform.Translate(Vector3.forward * 0.01f);
-                } else {
-                    goForward = false;
-                    goLeft = true;
-                }
-            } else if (goLeft) {
-                if (currentPosition.x > finalPosition.x) {
-                    transform.Translate(Vector3.left * 0.01f);
-                } else {
-                    goLeft = false;
-                }
-            } else {
-                transform.Rotate(0, 0, 90);
-                finalPos = true;
-            }
-        }
     }
 }
